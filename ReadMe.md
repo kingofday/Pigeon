0d785c968e4d25edca7cf8e975fcff8170a4fc61
BazrAfshan-PC <shahrooz.bazrafshan@gmail.com>
2018-10-23 12:27:26 +0330

    init

---
+++ ReadMe.md
@@ -0,0 +1,39 @@
+
+### Requirements
+
+* JQuery
+* bootstrap.js
+* bootstrap.css
+
+### Install
+```
+add "css/pigeon.css" to head tag in html page
+add "css/bootstrap.css" to head tag in html page
+add "js/jquery-3.1.1.min.js"(version not matters) to end of body tag
+add "js/pigeon.js" to end of body tag to
+```
+### Usage
+```
+define a button with unique id
+init plugin like this: 
+$('select your button').pigeon({
+    url:"your api address",
+    //other params
+});
+```
+
+### Options
+
+| option | type | desc |
+| --- | --- |--- |
+|  url | string |  url of server api for sending each file seperatly
+| maxFileCount | int | maximum number of files to upload, default is 10 |
+| maxFileSize | int | maximum size of each file in MB, default is 5 |
+
+### Some Points
+
+* each file will be sent by key "file"
+* its better to wrap button in a div with class "col-x(>3)"
+* server reponse should be in json format with this props
+* 1- IsSuccesssful
+* 2- Message: message if there is an error
---
+++ js/pigeon.js
@@ -0,0 +1,158 @@
+﻿/*! 
+ * Copyright (c) 2018 kingofday.ir
+ * 
+ * multi file uploader like google
+ * 
+ * Author: shahrooz.bazrafshan@gmail.com
+ * 
+ * Version: 1.1.1
+ *
+ */
+var pigeonDic = {
+    defaultError: "operation failed Retry please",
+    loading: "loading ...",
+    uploadDone: 'uploaded',
+    maxFileCountReached: 'maximum number of files is {0}.',
+    maxFileSizeExceeded: 'maximum size of each file is {0}',
+};
+(function ($) {
+    $.fn.pigeon = function (args) {
+        var $elm = $(this);
+        //default opt
+        args = typeof args !== 'undefined' ? args : {};
+        args.url = typeof args.url !== 'undefined' ? args.url : $elm.data('url');
+        args.maxFileCount = typeof args.maxFileCount !== 'undefined' ? args.maxFileCount : 10;
+        args.maxFileSize = typeof args.maxFileSize !== 'undefined' ? args.maxFileSize : 5;//MB
+        args.extentions = typeof args.extentions !== 'undefined' ? args.extentions : '*';
+        var id = $elm.attr('id');
+        //add wrapper
+        $('#' + id + '-pigeon-wrapper').remove();
+        var $wrapper = $('<div></div>', { class: 'pigeon-wrapper', id: id + '-pigeon-wrapper' });
+        var $parent = $elm.parent().append($wrapper);
+        var $input = $('<input/>', {
+            type: 'file',
+            class: 'pigeon-input',
+            id: id + 'pigeon-input',
+            multiple: args.multiple,
+            accept: args.extentions
+
+        });
+        $wrapper.append($input);
+        //
+        var toolTipTemplate = '<div class="tooltip {0}" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>';
+        //
+        var filesCount = 0;
+        $input.off('change').on('change', function (e) {
+            var files = e.target.files;
+            if (FileReader && files && files.length) {
+                //check file count
+                if (files.length + filesCount > args.maxFileCount) {
+                    $elm.tooltip({
+                        title: pigeonDic.maxFileCountReached.replace('{0}', args.maxFileCount),
+                        trigger: 'manual',
+                        placement: 'top',
+                        template: toolTipTemplate.replace('{0}', 'danger')
+                    });
+                    $elm.tooltip('show');
+                    return;
+                }
+                //check files size
+                if (Object.keys(files).some(function (k) { return files[k].size > (args.maxFileSize * 1024 * 1024) })) {
+                    $elm.tooltip({
+                        title: pigeonDic.maxFileSizeExceeded.replace('{0}', args.maxFileSize),
+                        trigger: 'manual',
+                        placement: 'top',
+                        template: toolTipTemplate.replace('{0}', 'danger')
+                    });
+                    $elm.tooltip('show');
+                    return;
+                }
+                for (var i = 0; i < files.length; i++) {
+                    upload(files[i]);
+                }
+
+            }
+        });
+
+        $elm.off('click').on('click', function () { $input.val(null).trigger('click') });
+        //remove tooltip
+        $(document).on('click', function () { $elm.tooltip('dispose'); });
+        //upload each file to server & append loader
+        var upload = function (file) {
+            var data = new FormData();
+            var request = new XMLHttpRequest();
+
+            data.append('file', file);
+            //add record
+            let $record = $('<div></div>', {
+                id: new Date().getUTCMilliseconds(),
+                class: 'pigeon-record',
+                title: pigeonDic.loading,
+                "data-toggle": "tooltip",
+                "data-placement": "top",
+            });
+            //get response
+            request.onreadystatechange = function (oEvent) {
+                if (request.readyState === 4) {
+                    if (request.status === 200) {
+                        if (request.response.IsSuccessful) {
+                            filesCount++;
+                            if (typeof args.success !== 'undefined') args.success(request.response);
+                            setState($record, pigeonDic.uploadDone, true);
+                        }
+                        else {
+                            setState($record, request.response.Message, false);
+                        }
+                        console.log(request.response);
+                    } else {
+                        $record.attr('title', pigeonDic.defaultError).addClass('danger');
+                        //showErrorMessage($record, 'بده دو');
+                        setState($record, pigeonDic.defaultError, false);
+                    }
+                }
+            };
+            //add prgressbar
+            $record.append($('<a></a>', {
+                class: 'pigeon-details',
+                title: file.name
+            }).html('<span class="name">' + file.name.substring(0, Math.min(file.name.length, 15)) + '</span>' + '(' + Math.floor(file.size / (1024)) + 'KB)'));
+            $record.append('<div class="progress-wrapper"><div class="progress"></div></div>');
+            let $progress = $('<div></div>', {
+                class: 'progress-bar progress-bar-success',
+                style: 'width:1%',
+                role: 'progressbar',
+                aria_valuenow: '1',
+                aria_valuemin: '0',
+                aria_valuemax: '100'
+            });
+            $record.find('.progress').append($progress);
+            $wrapper.append($record);
+            request.upload.addEventListener('progress', function (e) {
+                var p = (e.loaded / e.total) * 100;
+                $progress.css({ width: p + '%' });
+            });
+            request.responseType = 'json';
+
+            // Send POST request to the server side script
+            request.open('post', args.url);
+
+            request.send(data);
+        };
+        //set tooltip for each file
+        var setState = function ($elm, message, success) {
+            $elm.attr('title', message).addClass(success ? 'success' : 'danger');
+            $elm.find('.progress-wrapper').remove();
+            var $i = $('<i class="zmdi zmdi-' + (success ? 'check' : 'close') + '"></i>');
+            $elm.append($i);
+            $('#' + $elm.attr('id')).tooltip({
+                template: toolTipTemplate.replace('{0}', (success ? 'success' : 'danger'))
+            });
+            if (!success) {
+                $i.on('click', function () {
+                    $(this).closest('.pigeon-record').tooltip('dispose').remove();
+                });
+            }
+        };
+        return this;
+    };
+}(jQuery));
